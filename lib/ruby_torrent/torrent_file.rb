2d863b5f3f2ba593ed6253dc194df34a83763761
Georgi K <georgiknox@gmail.com>
2014-07-02 23:11:52 -0400

    moved bulk of torrent file related logic to TorrentFile class from client

--- lib/ruby_torrent/torrent_file.rb
+++ lib/ruby_torrent/torrent_file.rb
@@ -15,6 +15,7 @@ class TorrentFile
   HANDSHAKE_RESERVED = "\x00\x00\x00\x00\x00\x00\x00\x00"
 
   FileToDownload = Struct.new(:length, :path)
+  PeerAddress = Struct.new(:host, :port)
 
   attr_reader :announce, :length, :peer_id, :info_hash, :complete
 
@@ -58,8 +59,83 @@ class TorrentFile
     @length ||= calculate_length
   end
 
+  def connect_with_tracker_and_peers
+    tracker_response = connect_to_tracker
+    @peers = init_peers(tracker_response)
+    send_handshake_to_peers
+  end
+
+  #TODO: Event Machine for HTTP get?
+  def connect_to_tracker
+    # connect via HTTP to tracker
+    # tracker hold info about torrent and peers
+    # will respond to get request with list of peers
+    # PARAMS
+    # info_hash -> Compute SHA1 hash on bencoded info dict ONLY. ensure order is preserved
+    # peer_id -> anything that is 20 bytest long
+    # left -> for first time should be total length of file.
+    uri = build_tracker_request_uri
+    res = Net::HTTP.get_response(uri)
+
+    if (res.is_a?(Net::HTTPSuccess))
+      return res.body
+    else
+      puts "Cannot connect to torrent tracker."
+      return -1
+    end
+  end
+
+  def build_tracker_request_uri
+    uri = URI(@announce)
+    params = {:info_hash => @info_hash,
+              :peer_id => @peer_id,
+              :left => @length.to_s
+    }
+    uri.query = URI.encode_www_form(params)
+    puts "Tracker URI is: #{uri}"
+    return uri
+  end
+
+  # peers: (binary model) Instead of using the dictionary model described above,
+  # the peers value may be a string consisting of multiples of 6 bytes.
+  # First 4 bytes are the IP address and last 2 bytes are the port number.
+  # All in network (big endian) notation.
+  #TODO: refactor, smaller and should this be all in one class?
+  def init_peers(tracker_response)
+    peers_hash = Encoder.decode(tracker_response)
+    peers = peers_hash["peers"]
+    num_hosts = peers_hash["complete"] + peers_hash["incomplete"]
+
+    peers_array = []
+    peers.each_byte do |b|
+      peers_array << b
+    end
+
+    ip_addresses = []
+    num_hosts.times {
+      ip_address = peers_array.shift(4).join(".")
+      port = (peers_array.shift * 256) + peers_array.shift
+      ip_addresses << PeerAddress.new(ip_address.to_s, port)
+    }
+    return ip_addresses
+  end
+
+  def send_handshake_to_peers
+    @peers.each do |peer|
+      begin
+        puts "peer host #{peer.host}:#{peer.port} "
+        EM.connect(peer.host, peer.port, Peer, peer.host, peer.port, handshake_message)
+      rescue EventMachine::ConnectionError
+        puts "error connecting to peer #{peer.host}:#{peer.port}"
+      end
+    end
+  end
+
   # TODO: should be refactored to own class?
-  def construct_handshake_message
+  # The length prefix is a four byte big-endian value.
+  # The message ID is a single decimal byte.
+  # The payload is message dependent.
+  def handshake_message
     HANDSHAKE_PSTRLEN + HANDSHAKE_PSTR + HANDSHAKE_RESERVED + info_hash + peer_id
   end
 
